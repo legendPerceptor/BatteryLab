03efe9236ce29322ed402ab23face2791b0961db
Yuanjian Rasp4b <yuanjian@uchicago.edu>
2024-08-06 16:50:43 -0500

    fix the launch file to use python in the virtual environment

--- ros2_ws/src/battery_lab_bringup/launch/zaberrasp.launch.py
+++ ros2_ws/src/battery_lab_bringup/launch/zaberrasp.launch.py
@@ -1,11 +1,16 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.substitutions import LaunchConfiguration
+from launch.actions import SetEnvironmentVariable
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    venv_path = "/home/yuanjian/Research/BatteryLab/lab_venv"
+
     return LaunchDescription([
+        SetEnvironmentVariable('PATH', os.path.join(venv_path, 'bin') + ':' + os.environ['PATH']),
+        # Set the PYTHONPATH to include the virtual environment's site-packages
+        SetEnvironmentVariable('PYTHONPATH', os.path.join(venv_path, 'lib', 'python3.12', 'site-packages')), 
         Node(
             package='linear_rail_control',
             executable='linear_rail_server',
