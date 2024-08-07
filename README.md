3a03b8ffe35e287c407d1182b5a520ffd10a960d
Yuanjian Liu <yuanjian@uchicago.edu>
2024-08-06 23:07:42 -0500

    use cv_bridge to run the camera modules

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BatteryLab
 We aim to build an autonomous laboratory for building coin-cell batteries.
+
+## Known Issues
+
+### 1. prebuilt cv_bridge package is not compatible with Numpy 2.0
+
+We added a submodule to build the cv_bridge from source to fix this problem. You need to install the Boost Python library to build this package. The required packages can be installed by the following commands.
+
+```bash
+sudo apt update
+sudo apt install libboost-all-dev
+```
+
+And because we have a submodule in the repo. You may need to fetch the submodules before running `colcon build`.
+
+```bash
+git submodule update --init --recursive
+```
---
+++ ros2_ws/src/camera_service/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'camera_service'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yuanjian',
+    maintainer_email='yuanjian@uchicago.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "camera_server = camera_service.camera_server:main",
+            "camera_client = camera_service.camera_client:main"
+        ],
+    },
+)
