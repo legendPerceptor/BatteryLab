05d7c200594bb5fe47f399876831fcf3d9d65d2c
Yuanjian Liu <yuanjian@uchicago.edu>
2024-10-30 18:01:35 -0500

    Delay 1s for smart grab

--- BatteryLab/robots/RailMeca500.py
+++ BatteryLab/robots/RailMeca500.py
@@ -5,14 +5,37 @@ from .Meca500 import Meca500
 
 from pathlib import Path
 
+
 class RailMeca500(Meca500):
 
-    def __init__(self, logger = None, log_path="logs", logger_filename="Meca500.log", robot_address="192.168.0.101",
-                 suction_pump = None,
-                 robot_constants_config_file=Path(__file__).parent.parent / "configs" / "RailMeca500.yaml"):
-        super().__init__(logger, log_path, logger_filename, robot_address, robot_constants_config_file)
+    def __init__(
+        self,
+        logger=None,
+        log_path="logs",
+        logger_filename="Meca500.log",
+        robot_address="192.168.0.101",
+        suction_pump=None,
+        robot_constants_config_file=Path(__file__).parent.parent
+        / "configs"
+        / "RailMeca500.yaml",
+    ):
+        super().__init__(
+            logger,
+            log_path,
+            logger_filename,
+            robot_address,
+            robot_constants_config_file,
+        )
         self.status = {}
-        self.suction_pump = suction_pump if suction_pump is not None else SuctionPump(self.logger, self.status, vacuum_port=get_proper_port_for_device(SupportedDevices.SuctionPump))
+        self.suction_pump = (
+            suction_pump
+            if suction_pump is not None
+            else SuctionPump(
+                self.logger,
+                self.status,
+                vacuum_port=get_proper_port_for_device(SupportedDevices.SuctionPump),
+            )
+        )
         ok = self.suction_pump.connect_pump()
         if not ok:
             self.logger.error("Cannot connect to the suction pump!")
@@ -34,7 +57,6 @@ class RailMeca500(Meca500):
             self.home = self.RobotConstants.HOME_CA_J
         self.tool = tool_name
 
-
     def smart_grab(self):
         if self.tool == RobotTool.GRIPPER:
             self.robot.SetGripperForce(5)
@@ -44,21 +66,28 @@ class RailMeca500(Meca500):
             self.suction_pump.continues_pick()
         else:
             self.logger.error("invalid tool for smart grab!")
-    
+
     def smart_drop(self):
         if self.tool == RobotTool.GRIPPER:
             self.robot.SetGripperForce(5)
             self.robot.GripperOpen()
             self.robot.WaitGripperMoveCompletion(10)
-        elif self.tool== RobotTool.SUCTION:
+        elif self.tool == RobotTool.SUCTION:
             self.suction_pump.drop()
         else:
             self.logger.error("invalid tool for smart grab!")
 
-    def pick_place(self, grab_pos, is_grab = True):
+    def pick_place(self, grab_pos, is_grab=True):
         self.logger.info(f"Starting picking component at {grab_pos}")
         self.robot.SetJointVel(self.RobotConstants.J_VEL)
-        self.robot.MovePose(grab_pos[0], grab_pos[1], grab_pos[2] + 30, grab_pos[3], grab_pos[4], grab_pos[5])
+        self.robot.MovePose(
+            grab_pos[0],
+            grab_pos[1],
+            grab_pos[2] + 30,
+            grab_pos[3],
+            grab_pos[4],
+            grab_pos[5],
+        )
         # Linearly moving down to grab the component and go back
         self.robot.Delay(0.5)
         self.robot.SetCartLinVel(self.RobotConstants.L_VEL)
@@ -68,27 +97,46 @@ class RailMeca500(Meca500):
             self.smart_grab()
         else:
             self.smart_drop()
-        self.robot.Delay(0.2)
-        self.robot.MoveLin(grab_pos[0], grab_pos[1], grab_pos[2] + 30, grab_pos[3], grab_pos[4], grab_pos[5])
+        self.robot.Delay(1)
+        self.robot.MoveLin(
+            grab_pos[0],
+            grab_pos[1],
+            grab_pos[2] + 30,
+            grab_pos[3],
+            grab_pos[4],
+            grab_pos[5],
+        )
         self.robot.Delay(0.5)
         # Move the component back to home
         self.robot.SetCartLinVel(self.RobotConstants.L_VEL)
         self.move_home(tool=self.tool)
         self.robot.WaitIdle()
-    
+
     def move_to_pick_position(self, grab_pos, level: float = 1):
         if level > 1 or level < 0:
-            self.logger.error("The pick position level has to be a number in the range [0, 1]")
+            self.logger.error(
+                "The pick position level has to be a number in the range [0, 1]"
+            )
             return
         self.logger.info(f"Moving to component at {grab_pos} for manual adjustment.")
         self.robot.SetJointVel(self.RobotConstants.J_VEL)
-        self.robot.MovePose(grab_pos[0], grab_pos[1], grab_pos[2] + 30 * level, grab_pos[3], grab_pos[4], grab_pos[5])
+        self.robot.MovePose(
+            grab_pos[0],
+            grab_pos[1],
+            grab_pos[2] + 30 * level,
+            grab_pos[3],
+            grab_pos[4],
+            grab_pos[5],
+        )
         self.robot.WaitIdle()
+
 
 def rail_meca500_example_app():
     robot_address = "192.168.0.100"
-    user_provided_address = input(f"Please type in the IP address for the robot (the default is {robot_address}, press enter to use the default): ")
-    if user_provided_address != '':
+    user_provided_address = input(
+        f"Please type in the IP address for the robot (the default is {robot_address}, press enter to use the default): "
+    )
+    if user_provided_address != "":
         robot_address = user_provided_address
     meca500_robot = RailMeca500(robot_address=robot_address)
     meca500_robot.initializeRobot()
