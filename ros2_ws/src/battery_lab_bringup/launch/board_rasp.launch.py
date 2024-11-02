77528c7f39dcef149a0483bfef40da1a79de6b35
Yuanjian Liu <yuanjian@uchicago.edu>
2024-11-02 16:00:27 -0500

    fix the board rasp launch file with launch argument

--- ros2_ws/src/battery_lab_bringup/launch/board_rasp.launch.py
+++ ros2_ws/src/battery_lab_bringup/launch/board_rasp.launch.py
@@ -2,7 +2,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import SetEnvironmentVariable, DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import PythonExpression
+from launch.substitutions import LaunchConfiguration
 from pathlib import Path
 
 
@@ -16,12 +16,19 @@ def generate_launch_description():
             # SetEnvironmentVariable('PATH', os.path.join(venv_path, 'bin') + ':' + os.environ['PATH']),
             # # Set the PYTHONPATH to include the virtual environment's site-packages
             # SetEnvironmentVariable('PYTHONPATH', os.path.join(venv_path, 'lib', 'python3.12', 'site-packages')),
+            DeclareLaunchArgument(
+                "rail_velocity_mm_s",
+                default_value="40.0",
+                description="The velocity for the Zaber linear rail",
+            ),
             Node(
                 package="linear_rail_control",
                 executable="linear_rail_server",
                 name="linear_rail_server",
                 output="screen",
-                parameters=[{"rail_velocity_mm_s", 40}],
+                parameters=[
+                    {"rail_velocity_mm_s", LaunchConfiguration("rail_velocity_mm_s")}
+                ],
             ),
             Node(
                 package="camera_service",
